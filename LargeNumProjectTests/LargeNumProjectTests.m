5398b277663868d59a28ad4a9cddc7e5a17b44bb
Xander Li <x@xanderli.com>
2017-02-22 09:57:52 -0500

    Added some unit tests and performance tests

--- LargeNumProjectTests/LargeNumProjectTests.m
+++ LargeNumProjectTests/LargeNumProjectTests.m
@@ -7,7 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "LargeInt.h"
+#import "LargeNumGenerator.h"
+#import "LargeIntCalculator.h"
 @interface LargeNumProjectTests : XCTestCase
 
 @end
@@ -29,9 +31,42 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
+- (void)testLargeIntInit{
+    
+}
+- (void)testIsGreaterThan{
+    
+}
+- (void)testIsLessThan{
+    
+}
+- (void)testIsEqual{
+    
+}
+- (void)testAddition{
+    
+}
+- (void)testSubtraction{
+    
+}
+- (void)testMultiply{
+    
+}
+- (void)testDivide{
+    
+}
+- (void)testRemainder{
+    
+}
 - (void)testPerformanceExample {
+    LargeIntCalculator *calc = [[LargeIntCalculator alloc] init];
+    LargeNumGenerator *gen = [[LargeNumGenerator alloc] init];
+    LargeInt *test1 = [gen generateLargeIntWithLength:1000];
+    LargeInt *test2 = [gen generateLargeIntWithLength:2000];
+    
     // This is an example of a performance test case.
     [self measureBlock:^{
+        LargeInt *result = [calc multiply:test1 by:test2];
         // Put the code you want to measure the time of here.
     }];
 }
