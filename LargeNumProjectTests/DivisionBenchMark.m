48a050f21d71e6724611a12d1d4b74350c79d071
Xander Li <x@xanderli.com>
2017-03-03 01:35:44 -0500

    More benchmark

--- LargeNumProjectTests/DivisionBenchMark.m
+++ LargeNumProjectTests/DivisionBenchMark.m
@@ -7,33 +7,98 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "LargeIntCalculator.h"
+#import "LargeNumGenerator.h"
 @interface DivisionBenchMark : XCTestCase
 
 @end
 
-@implementation DivisionBenchMark
+@implementation DivisionBenchMark{
+    LargeIntCalculator *calc;
+    LargeInt *test1;
+    LargeInt *test2;
+    LargeInt *result;
+    LargeNumGenerator *gen;
+}
 
 - (void)setUp {
     [super setUp];
+    
+    calc = [[LargeIntCalculator alloc] init];
+    test1 = [[LargeInt alloc] init];
+    test2 = [[LargeInt alloc] init];
+    result = [[LargeInt alloc] init];
+    gen = [[LargeNumGenerator alloc] init];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
+    calc = nil;
+    test1 = nil;
+    test2 = nil;
+    result = nil;
+    gen = nil;
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
+- (void)test10Dig {
+    test1 = [gen generateLargeIntWithLength:10];
+    test2 = [gen generateLargeIntWithLength:10];
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        result = [calc divide:test1 by:test2];
     }];
 }
+
+- (void)test100Dig {
+    test1 = [gen generateLargeIntWithLength:100];
+    test2 = [gen generateLargeIntWithLength:100];
+    [self measureBlock:^{
+        result = [calc divide:test1 by:test2];
+    }];
+}
+- (void)test1000Dig {
+    test1 = [gen generateLargeIntWithLength:1000];
+    test2 = [gen generateLargeIntWithLength:1000];
+    [self measureBlock:^{
+        result = [calc divide:test1 by:test2];
+    }];
+}
+- (void)test10000Dig {
+    test1 = [gen generateLargeIntWithLength:10000];
+    test2 = [gen generateLargeIntWithLength:10000];
+    [self measureBlock:^{
+        result = [calc divide:test1 by:test2];
+    }];
+}
+//- (void)test100000Dig {
+//    test1 = [gen generateLargeIntWithLength:100000];
+//    test2 = [gen generateLargeIntWithLength:100000];
+//    [self measureBlock:^{
+//        result = [calc divide:test1 by:test2];
+//    }];
+//}
+//- (void)test500000Dig {
+//    test1 = [gen generateLargeIntWithLength:500000];
+//    test2 = [gen generateLargeIntWithLength:500000];
+//    [self measureBlock:^{
+//        result = [calc divide:test1 by:test2];
+//    }];
+//}
+//- (void)test1000000Dig {
+//    test1 = [gen generateLargeIntWithLength:1000000];
+//    test2 = [gen generateLargeIntWithLength:1000000];
+//    [self measureBlock:^{
+//        result = [calc divide:test1 by:test2];
+//    }];
+//}
+//- (void)test10000000Dig {
+//    test1 = [gen generateLargeIntWithLength:10000000];
+//    test2 = [gen generateLargeIntWithLength:10000000];
+//    [self measureBlock:^{
+//        result = [calc divide:test1 by:test2];
+//    }];
+//}
 
 @end
